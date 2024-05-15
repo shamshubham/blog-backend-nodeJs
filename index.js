4918c1f8adc7838428626cf8e3545dc745aeec04
shubhamverma <sham.shubham19@gmail.com>
2024-05-15 12:28:40 +0530

    Basic setup and creating models

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+// const authRoute = require("./src/routes/auth");
+
+dotenv.config();
+app.use(express.json());
+
+const PORT = 8000;
+
+// Connect database MongoDB
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+// Routes
+
+// app.use("/shubham", (req, res) => {
+//   console.log("Hey this is shubham url");
+// });
+
+// app.use("/api/v1/auth", authRoute);
+
+// Creating Server
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
+});
