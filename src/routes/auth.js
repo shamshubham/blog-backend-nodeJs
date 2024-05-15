33386d0ce20f612da0b5db2c65f1613ca636ccac
shubhamverma <sham.shubham19@gmail.com>
2024-05-15 18:22:01 +0530

    login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -22,3 +22,24 @@ router.post("/register", async (req, res) => {
 });
 
 module.exports = router;
+
+// Login
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(400).json("Wrong credentials!");
+    }
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    if (!validated) {
+      return res.status(400).json("Wrong credentials!");
+    }
+
+    const { password, ...others } = user._doc;
+    res.status(200).json({ msg: "Login successfull", others });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
